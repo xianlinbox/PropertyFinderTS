02dfc1603ea18fd4f4c6d1f046e85e07ab69dcd7
xianlinbox <xianlinbox@gmail.com>
2017-09-24 20:40:10 +0800

    rename component test to tsx suffix

---
+++ src/property/scenes/__test__/propertySearchScene.spec.tsx
@@ -0,0 +1,17 @@
+import {shallow} from 'enzyme';
+import {SearchPage} from '../propertySearchScene';
+import * as React from 'react';
+
+
+describe('searchPage', () => {
+  it('', () => {
+    let searchPage = shallow(<SearchPage
+      isLoading={false}
+      request={{place_name: 'London'}}
+      search={(str) => {console.log(str)}}
+    />);
+
+    expect(searchPage.find()).toBe('London');
+
+  })
+});
